c0f421f2f20f0b729fcb647c0f1ff55dec8168d8
Sarah <s.f.alashban@gmail.com>
2020-03-29 21:37:09 +0300

    Profile and Cart stuff

--- DonationBackend/urls.py
+++ DonationBackend/urls.py
@@ -14,8 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import UserCreateAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('profile/', views.ProfileDetails.as_view(), name="profile-details"),
+    # path('cart/', views.CartDetail.as_view(), name="cart"),
+    # path('cart/item/', views.CartItem.as_view(), name="cart_item"),
+    # path('cart/<int:cartitem_id>/update/', views.UpdateCart.as_view(), name="update-cartitem"),
+    # path('cart/<int:cartitem_id>/delete/', views.DeleteCartItem.as_view(), name="delete-cartitem"),
+    # path('checkout/', views.Checkout.as_view(), name="checkout"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
--- api/models.py
+++ api/models.py
@@ -9,3 +9,24 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+
+# class Cart_Item(models.Model):
+#     item = models.ForeignKey(
+#         Item, on_delete=models.CASCADE)
+#     quantity = models.PositiveIntegerField()
+
+#     def __str__(self):
+#         return "%s: %s" % (self.item.name, str(self.quantity))
+
+
+# class Cart(models.Model):
+#     user = models.ForeignKey(
+#         User, on_delete=models.CASCADE)
+#     cart_item = models.ForeignKey(
+#         Cart_Item, on_delete=models.CASCADE)
+#     date = models.DateField(auto_now_add=True)
+#     active = models.BooleanField()
+
+#     def __str__(self):
+#         return self.user.username
