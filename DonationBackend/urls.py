0f9c50729412f234d08c2c0fd21e9a42fcace4ba
Sarah <s.f.alashban@gmail.com>
2020-03-30 17:42:48 +0300

    Fix

--- DonationBackend/urls.py
+++ DonationBackend/urls.py
@@ -17,11 +17,8 @@ from django.contrib import admin
 from django.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
-<<<<<<< HEAD
 from api.views import UserCreateAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView
-=======
->>>>>>> a79e006dd1d6e9313decef917af7d38e0c42ff33
 from api import views
 
 urlpatterns = [
